0abc8004a1bbb2a956d4e60b6e6705a138050b66
Molly Gallagher Wickham <mollyg210@gmail.com>
2023-07-31 17:03:38 +0100

    started github-finder project

---
+++ React-Front-to-Back/github-finder/src/pages/Home.jsx
@@ -0,0 +1,12 @@
+import UserResults from "../components/users/UserResults"
+
+function Home() {
+    return (
+        <>
+            {/* SEARCH COMPONENT HERE */}
+            <UserResults />
+        </>
+    )
+}
+
+export default Home
